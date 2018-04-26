bc8ceb53b8da7c54ac3de0b681f531d397ce6c47
xurendong <xrdlyj@163.com>
2018-04-26 15:15:49 +0800

    add test analysis

---
+++ src/anal/analysis_test.py
@@ -0,0 +1,48 @@
+
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2018 the QuantX authors
+# All rights reserved.
+#
+# The project sponsor and lead author is Xu Rendong.
+# E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+# You can get more information at https://xurendong.github.io
+# For names of other contributors see the contributors file.
+#
+# Commercial use of this code in source and binary forms is
+# governed by a LGPL v3 license. You may get a copy from the
+# root directory. Or else you should get a specific written 
+# permission from the project author.
+#
+# Individual and educational use of this code in source and
+# binary forms is governed by a 3-clause BSD license. You may
+# get a copy from the root directory. Certainly welcome you
+# to contribute code of all sorts.
+#
+# Be sure to retain the above copyright notice and conditions.
+
+import logger
+import basicx
+import analysis_base
+
+class AnalysisTest(analysis_base.AnalysisBase):
+    def __init__(self):
+        analysis_base.AnalysisBase.__init__(self, "AnalysisTest", "Test", "测试")
+        self.log_text = ""
+        self.log_cate = "AnalysisTest"
+        self.logger = logger.Logger()
+        self.basicx = basicx.BasicX()
+
+    def OnWorking(self): # 供具体回测继承调用，在 运行 前执行一些操作
+        result = self.basicx.GetStockDaily("sz", "000001", 20100101, 20171231)
+        if not result.empty:
+            print(result)
+
+    def OnSuspend(self): # 供具体回测继承调用，在 暂停 前执行一些操作
+        pass
+
+    def OnContinue(self): # 供具体回测继承调用，在 继续 前执行一些操作
+        pass
+
+    def OnTerminal(self): # 供具体回测继承调用，在 停止 前执行一些操作
+        pass
