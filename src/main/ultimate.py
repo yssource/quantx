f1818383893ab60a8cf7a8e08d10457900514217
xurendong <xrdlyj@163.com>
2018-06-07 10:37:24 +0800

    add ultimate

---
+++ src/main/ultimate.py
@@ -0,0 +1,28 @@
+
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2018 the QuantX authors
+# All rights reserved.
+#
+# The project sponsor and lead author is Xu Rendong.
+# E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+# You can get more information at https://xurendong.github.io
+# For names of other contributors see the contributors file.
+#
+# Commercial use of this code in source and binary forms is
+# governed by a LGPL v3 license. You may get a copy from the
+# root directory. Or else you should get a specific written 
+# permission from the project author.
+#
+# Individual and educational use of this code in source and
+# binary forms is governed by a 3-clause BSD license. You may
+# get a copy from the root directory. Certainly welcome you
+# to contribute code of all sorts.
+#
+# Be sure to retain the above copyright notice and conditions.
+
+try:
+    import panel_basket_stk_ape # 只是为了打包时能被编译到
+    import panel_spread_fue_ctp # 只是为了打包时能被编译到
+    import panel_algorithm_stk_ape # 只是为了打包时能被编译到
+except: pass
--- src/main/window.py
+++ src/main/window.py
@@ -51,15 +51,11 @@ import strate
 
 import basic_data_maker
 
+import ultimate
 import strategy_base # 只是为了打包时能被编译到
 import analysis_base # 只是为了打包时能被编译到
 import panel_trader_stk_ape # 只是为了打包时能被编译到
 import panel_trader_fue_ctp # 只是为了打包时能被编译到
-
-try:
-    import panel_basket_stk_ape # 只是为了打包时能被编译到
-    import panel_spread_fue_ctp # 只是为了打包时能被编译到
-except: pass
 
 class MainWindow(QMainWindow):
     def __init__(self):
